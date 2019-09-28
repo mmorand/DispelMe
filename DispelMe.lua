57518c5be7959789cd8b4159be2105c3df52134d
Mathieu Morand <mathieu.morand@gammadia.ch>
2019-09-28 10:13:34 +0200

    Refactoring constants

--- DispelMe.lua
+++ DispelMe.lua
@@ -5,58 +5,82 @@ local _, namespace = ...;
 
 local Dm_MainContainer = {}
 local Dm_UnitFrames = {}
-local CONSTANTS = {
-    BOXES = {
-        GUTTER = 10,
-        HEIGHT = 20,
-        PERLINE = 5,
-        WIDTH = 50
-    },
-    CONTAINER_TOP = 0
-}
-local PLAYER_ATTRIBUTES = {
+
+local BOXESGUTTER = 10
+local BOXESHEIGHT = 20
+local BOXESPERLINE = 5
+local BOXESWIDTH = 50
+
+local CONTAINERTOP = 0
+
+local DM_PLAYER = {
     CLASSNAME = false,
-    CANDISPEL = false,
+    CANDISPEL = {
+        Curse = false,
+        Disease = false,
+        Magic = false,
+        Poison = false
+    },
     SPELL1 = false,
     SPELL2 = false,
     PRIORITY1 = false,
     PRIORITY2 = false
 }
-local CLASS_COLORS = {
-    DRUID =     {r = 1.00, g = 0.49, b = 0.04 },
-    HUNTER =    {r = 0.67, g = 0.83, b = 0.45 },
-    MAGE =      {r = 0.25, g = 0.78, b = 0.92 },
-    PALADIN =   {r = 0.96, g = 0.55, b = 0.73 },
-    PRIEST =    {r = 1.00, g = 1.00, b = 1.00 },
-    ROGUE =     {r = 1.00, g = 0.96, b = 0.41 },
-    SHAMAN =    {r = 0.00, g = 0.44, b = 0.87 },
-    WARLOCK =   {r = 0.53, g = 0.53, b = 0.93 },
-    WARRIOR =   {r = 0.78, g = 0.61, b = 0.43 },
-}
+
+local SPELLPRIORITY = { 'Curse', 'Magic', 'Poison', 'Disease' }
+
 --[[
 @TODO AJOUTER LES ID DES AUTRES SORTS POUR TOUTES LES CLASSES
 ]]--
-local CLASS_SPELLS = {
+
+local DM_CLASSES = {
     DRUID = {
+        CANDISPEL = {
+            Curse = true,
+            Disease = false,
+            Poison = true
+        },
+        PRIORITY1 = 'Curse',
+        PRIORITY2 = 'Poison',
         SPELL1 = 2782,
         SPELL2 = 14253
     },
-    MAGE = {
-        SPELL1 = 475
-    },
     PALADIN = {
-
+        PRIORITY1 = 'Magic',
+        SPELL1 = 2782,
+    },
+    MAGE = {
+        PRIORITY1 = 'Curse',
+        SPELL1 = 2782
     },
     PRIEST = {
-        SPELL1 = 527,
-        SPELL2 = 528
+        PRIORITY1 = 'Magic',
+        PRIORITY2 = 'Disease',
+        SPELL1 = 2782,
+        SPELL2 = 14253
     },
     SHAMAN = {
-
-    },
-    WARLOCK = {
-        SPELL1 = 696
+        PRIORITY1 = 'Curse',
+        PRIORITY2 = 'Poison',
+        SPELL1 = 2782,
+        SPELL2 = 14253
     }
+}
+local DM_COLORS = {
+    Curse =     { r = 1.00, g = 0.00, b = 1.00 },
+    Disease =   { r = 1.00, g = 0.67, b = 0.20 },
+    Magic =     { r = 0.86, g = 0.08, b = 0.24 },
+    Poison =    { r = 0.50, g = 1.00, b = 0.00 },
+
+    DRUID =     { r = 1.00, g = 0.49, b = 0.04 },
+    HUNTER =    { r = 0.67, g = 0.83, b = 0.45 },
+    MAGE =      { r = 0.25, g = 0.78, b = 0.92 },
+    PALADIN =   { r = 0.96, g = 0.55, b = 0.73 },
+    PRIEST =    { r = 1.00, g = 1.00, b = 1.00 },
+    ROGUE =     { r = 1.00, g = 0.96, b = 0.41 },
+    SHAMAN =    { r = 0.00, g = 0.44, b = 0.87 },
+    WARLOCK =   { r = 0.53, g = 0.53, b = 0.93 },
+    WARRIOR =   { r = 0.78, g = 0.61, b = 0.43 },
 }
 
 function Dm_RoundValue(val, decimal)
@@ -70,9 +94,9 @@ end
 function Dm_GenerateMainFrame()
 
     Dm_MainContainer = CreateFrame("Frame", "Dm_MainContainer", UIParent)
-    local Dm_ContainerHeight = (40 / CONSTANTS.BOXES.PERLINE) * (CONSTANTS.BOXES.HEIGHT + CONSTANTS.BOXES.GUTTER) + CONSTANTS.BOXES.GUTTER
-    local Dm_ContainerWidth = CONSTANTS.BOXES.PERLINE * (CONSTANTS.BOXES.WIDTH + CONSTANTS.BOXES.GUTTER) + CONSTANTS.BOXES.GUTTER
-    Dm_MainContainer:SetSize(Dm_ContainerWidth, (Dm_ContainerHeight + CONSTANTS.CONTAINER_TOP))
+    local Dm_ContainerHeight = (40 / BOXESPERLINE) * (BOXESHEIGHT + BOXESGUTTER) + BOXESGUTTER
+    local Dm_ContainerWidth = BOXESPERLINE * (BOXESWIDTH + BOXESGUTTER) + BOXESGUTTER
+    Dm_MainContainer:SetSize(Dm_ContainerWidth, (Dm_ContainerHeight + CONTAINERTOP))
     Dm_MainContainer:SetPoint("CENTER")
 
     Dm_MainContainer:SetMovable(true)
@@ -96,36 +120,36 @@ function Dm_UpdateButton(target)
         Dm_UnitFrames[target].text:SetFont("Fonts\\ARIALN.ttf", 10, "OUTLINE")
         Dm_UnitFrames[target].text:SetAllPoints()
         Dm_UnitFrames[target].text:SetText(UnitName(target))
-        Dm_UnitFrames[target].text:SetWidth(CONSTANTS.BOXES.WIDTH - 5)
+        Dm_UnitFrames[target].text:SetWidth(BOXESWIDTH - 5)
         Dm_UnitFrames[target]:SetNormalFontObject("GameFontNormalSmall")
         Dm_UnitFrames[target]:SetAlpha(1)
 
         local Dm_classNameFr, Dm_className = UnitClass(target)
-        local cColors = CLASS_COLORS[Dm_className]
+        local ClassColors = DM_COLORS[Dm_className]
         Dm_UnitFrames[target].texture = Dm_UnitFrames[target]:CreateTexture("Dm_DispelButtonTexture"..target, "BACKGROUND")
         Dm_UnitFrames[target].texture:SetAllPoints()
-        Dm_UnitFrames[target].texture:SetColorTexture(cColors.r, cColors.g, cColors.b, 1)
+        Dm_UnitFrames[target].texture:SetColorTexture(ClassColors.r, ClassColors.g, ClassColors.b, 1)
     end
 end
 
 function Dm_GenerateButton(target, unitId)
 
     Dm_UnitFrames[target] = CreateFrame("Button", "Dm_DispelButton"..target, Dm_MainContainer, "SecureActionButtonTemplate")
-    if (PLAYER_ATTRIBUTES.SPELL1) then
+    if (DM_PLAYER.SPELL1) then
         Dm_UnitFrames[target]:SetAttribute("type1", "spell")
-        Dm_UnitFrames[target]:SetAttribute("spell", PLAYER_ATTRIBUTES.SPELL1)
+        Dm_UnitFrames[target]:SetAttribute("spell", DM_PLAYER.SPELL1)
         Dm_UnitFrames[target]:SetAttribute("target", target)
     end
-    if (PLAYER_ATTRIBUTES.SPELL2) then
+    if (DM_PLAYER.SPELL2) then
         Dm_UnitFrames[target]:SetAttribute("type2", "spell")
-        Dm_UnitFrames[target]:SetAttribute("spell", PLAYER_ATTRIBUTES.SPELL2)
+        Dm_UnitFrames[target]:SetAttribute("spell", DM_PLAYER.SPELL2)
         Dm_UnitFrames[target]:SetAttribute("target", target)
     end
-    local posX = (unitId % CONSTANTS.BOXES.PERLINE) * (CONSTANTS.BOXES.WIDTH + CONSTANTS.BOXES.GUTTER) + CONSTANTS.BOXES.GUTTER
-    local posY = math.floor(unitId / CONSTANTS.BOXES.PERLINE) * (CONSTANTS.BOXES.HEIGHT + CONSTANTS.BOXES.GUTTER) + CONSTANTS.BOXES.GUTTER
-    Dm_UnitFrames[target]:SetPoint("TOPLEFT", posX, -(posY + CONSTANTS.CONTAINER_TOP))
-    Dm_UnitFrames[target]:SetWidth(CONSTANTS.BOXES.WIDTH)
-    Dm_UnitFrames[target]:SetHeight(CONSTANTS.BOXES.HEIGHT)
+    local posX = (unitId % BOXESPERLINE) * (BOXESWIDTH + BOXESGUTTER) + BOXESGUTTER
+    local posY = math.floor(unitId / BOXESPERLINE) * (BOXESHEIGHT + BOXESGUTTER) + BOXESGUTTER
+    Dm_UnitFrames[target]:SetPoint("TOPLEFT", posX, -(posY + CONTAINERTOP))
+    Dm_UnitFrames[target]:SetWidth(BOXESWIDTH)
+    Dm_UnitFrames[target]:SetHeight(BOXESHEIGHT)
     Dm_UnitFrames[target]:SetAlpha(0)
     Dm_UnitFrames[target].targetIndex = unitId + 1
 
@@ -144,9 +168,10 @@ function Dm_TriggerAura(target)
 
     local hasDebuff, index = false, 0
     for i = 1, 16, 1 do
-        local name, _, _, type, _, etime = UnitDebuff(target, i, "HARMFUL")
-        if (name and etime and etime > 0) then
-            print(i, etime, name, type)
+        local _, _, _, type, _, etime = UnitDebuff(target, i, "HARMFUL")
+        if (etime and etime > 0) then
+
+            print(i, etime, type)
 
             --Magic/Poison/Disease/Curse
 
@@ -163,17 +188,17 @@ end
 
 function Dm_SetPlayerAttributes()
     local Dm_PlayerClassNameFR, Dm_PlayerClassName = UnitClass("PLAYER")
-    PLAYER_ATTRIBUTES.CLASSNAME = Dm_PlayerClassName
+    DM_PLAYER.CLASSNAME = Dm_PlayerClassName
 
-    local usable = IsUsableSpell(CLASS_SPELLS[PLAYER_ATTRIBUTES.CLASSNAME].SPELL1)
+    local usable = IsUsableSpell(DM_CLASSES[DM_PLAYER.CLASSNAME].SPELL1)
     if (usable) then
-        PLAYER_ATTRIBUTES.SPELL1 = CLASS_SPELLS[PLAYER_ATTRIBUTES.CLASSNAME].SPELL1
+        DM_PLAYER.SPELL1 = DM_CLASSES[DM_PLAYER.CLASSNAME].SPELL1
     end
 
-    if (CLASS_SPELLS[PLAYER_ATTRIBUTES.CLASSNAME].SPELL2) then
-        local usable = IsUsableSpell(CLASS_SPELLS[PLAYER_ATTRIBUTES.CLASSNAME].SPELL2)
+    if (DM_CLASSES[DM_PLAYER.CLASSNAME].SPELL2) then
+        local usable = IsUsableSpell(DM_CLASSES[DM_PLAYER.CLASSNAME].SPELL2)
         if (usable) then
-            PLAYER_ATTRIBUTES.SPELL2 = CLASS_SPELLS[PLAYER_ATTRIBUTES.CLASSNAME].SPELL2
+            DM_PLAYER.SPELL2 = DM_CLASSES[DM_PLAYER.CLASSNAME].SPELL2
         end
     end
 end
@@ -202,7 +227,7 @@ function Dm_OnLoad(self)
     self:RegisterEvent("GROUP_ROSTER_UPDATE")
 
     Dm_SetPlayerAttributes()
-    if (CLASS_SPELLS[PLAYER_ATTRIBUTES.CLASSNAME]) then
+    if (DM_CLASSES[DM_PLAYER.CLASSNAME]) then
         Dm_Init()
     end
 end
